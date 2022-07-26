1730689e9c67263bf1a6fe8a647cb28d623c8768
Jordan <jclemens24@hotmail.com>
2022-07-26 08:00:08 -0400

    Removed: unnecessary annotations

--- build/plugin.d.ts
+++ build/plugin.d.ts
@@ -2,23 +2,23 @@
 import { FastifyPluginAsync } from 'fastify';
 import { DataSource, DataSourceOptions } from 'typeorm';
 declare module 'fastify' {
-    interface FastifyInstance {
-        orm: DataSource & FastifyTypeormInstance.FastifyTypeormNamespace;
-    }
+  interface FastifyInstance {
+    orm: DataSource & FastifyTypeormInstance.FastifyTypeormNamespace;
+  }
 }
 declare namespace FastifyTypeormInstance {
-    interface FastifyTypeormNamespace {
-        [namespace: string]: DataSource;
-    }
+  interface FastifyTypeormNamespace {
+    [namespace: string]: DataSource;
+  }
 }
-/**
- * @typedef {DBConfigOptions}
- * @property {DataSource} connection - A new DataSource passed to plugin
- * @property {string} namespace - Optional namespace to declare multiple DataSources in your project
- */
+
 declare type DBConfigOptions = {
-    connection?: DataSource;
-    namespace?: string;
+  connection?: DataSource;
+  namespace?: string;
 } & Partial<DataSourceOptions>;
-declare const _default: FastifyPluginAsync<DBConfigOptions, import("http").Server, import("fastify").FastifyTypeProviderDefault>;
+declare const _default: FastifyPluginAsync<
+  DBConfigOptions,
+  import('http').Server,
+  import('fastify').FastifyTypeProviderDefault
+>;
 export default _default;
